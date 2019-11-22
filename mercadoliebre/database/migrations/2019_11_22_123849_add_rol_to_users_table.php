1f2bfa2390d0eb3505e124f962cbf30f236032af
aledh <alejandro@digitalhouse.com>
2019-11-22 09:43:55 -0300

    Nuevos campos en tabla users.

---
+++ mercadoliebre/database/migrations/2019_11_22_123849_add_rol_to_users_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddRolToUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('avatar', '300');
+            $table->smallInteger('rol')->default(0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('avatar');
+            $table->dropColumn('rol');
+        });
+    }
+}
